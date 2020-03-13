ca4fbd124cb6fe64e13df73e5cf63503641d5dbe
Vinayak Khare <khare.vinayak@gmail.com>
2020-03-12 23:25:24 -0400

    exclude dit

--- src/thingForm.js
+++ src/thingForm.js
@@ -16,3 +16,5 @@ const ThingForm = ({ createThing })=> {
       </section>
     );
   };
+
+  export default ThingForm;
--- src/userForm.js
+++ src/userForm.js
@@ -16,3 +16,5 @@ const UserForm = ({ createUser })=> {
       </section>
     );
   };
+
+export default UserForm;
